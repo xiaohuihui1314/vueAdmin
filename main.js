e6932eeb88d4f79f3eb522eba2e07a09c8254915
1243817329@qq.com <520huiHui>
2020-04-07 14:32:50 +0800

    1

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const getParents = (startTag, parentTagList = []) => {
   if (!(startTag instanceof HTMLElement)) return console.error('receive only HTMLElement');
   if ('BODY' !== startTag.parentElement.nodeName) {
-    const nodeName = startTag.nodeName.toLocaleLowerCase()
+    const nodeName = startTag.nodeName.toLocaleLowerCase();
     const classNameStr = startTag.className.trim();
-    let className = ''
+    let className = '';
     if(classNameStr){
       className = classNameStr.split(' ').map(i=>'.'+i).join('')
     }
     // parentTagList.push(startTag.parentNode)
-    parentTagList.push(nodeName+className)
-
+    parentTagList.push(nodeName+className);
     return getParents(startTag.parentNode, parentTagList)
   } else return parentTagList;
 };
-let path = event.path || (event.composedPath && event.composedPath());
+let path = event.path || (event.composedPath && event.composedPath()) || getParents()
 document.body.addEventListener('click', e => {
   const target = e.target;
   const parentList = getParents(target);
