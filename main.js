4cf0ccef153e3060e30a39bd05c4115437f23bc4
1243817329@qq.com <520huiHui>
2020-04-11 17:55:52 +0800

    1

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const getParents = (startTag, parentTagList = []) => {
+const getParentsPath = (startTag, parentTagList = []) => {
+  let path = event.path || (event.composedPath && event.composedPath())
+  if (!path) {
+    return path
+  }
+
   if (!(startTag instanceof HTMLElement)) return console.error('receive only HTMLElement');
-  if ('BODY' !== startTag.parentElement.nodeName) {
+  if ('HTML' !== startTag.parentElement.nodeName) {
     const nodeName = startTag.nodeName.toLocaleLowerCase();
     const classNameStr = startTag.className.trim();
     let className = '';
-    if(classNameStr){
-      className = classNameStr.split(' ').map(i=>'.'+i).join('')
+    if (classNameStr) {
+      className = classNameStr.split(' ').map(i => '.' + i).join('')
     }
     // parentTagList.push(startTag.parentNode)
-    parentTagList.push(nodeName+className);
-    return getParents(startTag.parentNode, parentTagList)
+    parentTagList.push(nodeName + className);
+    return getParentsPath(startTag.parentNode, parentTagList)
   } else return parentTagList;
 };
-let path = event.path || (event.composedPath && event.composedPath()) || getParents()
 document.body.addEventListener('click', e => {
   const target = e.target;
-  const parentList = getParents(target);
+  const parentList = getParentsPath(target);
   console.log(target);
   console.log(parentList);
 });
+
+
